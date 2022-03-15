56e765a546a9d93bae3d48715a1906173f981aec
saridraj <saridrajsapsin@gmail.com>
2022-03-15 16:53:13 +0700

    add fellow doc feature

--- components/FellowDoc.js
+++ components/FellowDoc.js
@@ -13,14 +13,14 @@ function FellowDoc({summaryDocs, proposalDocs, progressDocs, reflectionDocs }) {
                     <div className="w-[380px] h-auto  mt-2 mb-5">
                         <p className="text-[#404040]">เอกสารสรุปแนวคิดการพัฒนาระบบสุขภาพ <br/>(Executive summary)</p>
                         <p className="font-light text-[14px]">{summaryDocs.map((summaryDoc) => ((summaryDoc.Timestamp)))}</p>
-                        <a href={`${summaryDocs.map((summaryDoc) => ((summaryDoc.file)))}`} target="_blank" rel="noopener">
+                        <a href={`${summaryDocs.map((summaryDoc) => ((summaryDoc.file)))}`} target="_blank" rel="noreferrer">
                         <button className="bg-[#91466b] text-white w-[100px] h-[35px] rounded-[20px] mt-2">อ่าน</button>
                         </a>
                     </div>
                     <div className="w-[380px] h-auto  mt-2 mb-5">
                         <p className="text-[#404040]">เอกสารข้อเสนอโครงการ (Proposal)</p>
                         <p className="font-light text-[14px]">{proposalDocs.map((proposalDoc) => ((proposalDoc.Timestamp)))}</p>
-                        <a href={`${proposalDocs.map((proposalDoc) => ((proposalDoc.file)))}`} target="_blank" rel="noopener">
+                        <a href={`${proposalDocs.map((proposalDoc) => ((proposalDoc.file)))}`} target="_blank" rel="noreferrer">
                         <button className="bg-[#91466b] text-white w-[100px] h-[35px] rounded-[20px] mt-2">อ่าน</button>
                         </a>
                     </div>
@@ -33,14 +33,14 @@ function FellowDoc({summaryDocs, proposalDocs, progressDocs, reflectionDocs }) {
                     <div className="w-[380px] h-auto  mt-2 mb-5">
                         <p className="text-[#404040]">เอกสารสรุปความก้าวหน้า (Progress report)</p>
                         <p className="font-light text-[14px]">{progressDocs.map((progressDoc) => ((progressDoc.Timestamp)))}</p>
-                        <a href={`${progressDocs.map((progressDoc) => ((progressDoc.file)))}`} target="_blank" rel="noopener">
+                        <a href={`${progressDocs.map((progressDoc) => ((progressDoc.file)))}`} target="_blank" rel="noreferrer">
                         <button className="bg-[#91466b] text-white w-[100px] h-[35px] rounded-[20px] mt-2">อ่าน</button>
                         </a>
                     </div>
                     <div className="w-[380px] h-auto  mt-2 mb-5">
                         <p className="text-[#404040]">เอกสารสรุปบทเรียน (Reflection note)</p>
                         <p className="font-light text-[14px]">{reflectionDocs.map((reflectionDoc) => ((reflectionDoc.Timestamp)))}</p>
-                        <a href={`${reflectionDocs.map((reflectionDoc) => ((reflectionDoc.file)))}`} target="_blank" rel="noopener">
+                        <a href={`${reflectionDocs.map((reflectionDoc) => ((reflectionDoc.file)))}`} target="_blank" rel="noreferrer">
                         <button className="bg-[#91466b] text-white w-[100px] h-[35px] rounded-[20px] mt-2">อ่าน</button>
                         </a>
                     </div>
